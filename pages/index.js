83bf58e922ea7daedbcf8d6708397f5e887966f9
kunalraj1310 <kunalraj13102011@gmail.com>
2025-09-16 15:33:59 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Welcome to Jarvis UI</h1>
+      <p>Your frontend is connected successfully!</p>
+    </div>
+  );
+  }
